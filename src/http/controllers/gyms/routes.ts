26e567b8029f0e2ab02a96370236bfe5979dde04
Murilo Thom <murilothom.2002@hotmail.com>
2023-08-02 12:32:31 -0300

    validate user role

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 import { search } from './search'
 import { nearby } from './nearby'
@@ -9,8 +10,8 @@ import { create } from './create'
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
 
-  app.post('/gyms', create)
-
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
